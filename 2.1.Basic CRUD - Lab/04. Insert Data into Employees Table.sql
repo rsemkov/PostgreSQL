49d372fbae6885c69f1df4f20357673858a5ab4f
rsemkov <136321984+rsemkov@users.noreply.github.com>
2024-05-22 09:02:11 +0300

    SQL tasks upload

---
+++ 2.1.Basic CRUD - Lab/04. Insert Data into Employees Table.sql
@@ -0,0 +1,13 @@
+INSERT INTO employees(first_name, last_name, job_title, department_id, salary)
+ VALUES 
+	('Samantha', 'Young', 'Housekeeping', 4, 900),
+	('Roger', 'Palmer', 'Waiter', 3, 928.33);
+
+SELECT * from employees;
+
+-- Insert new records into table employees by writing a query. Select all employees’ info to check the new entries.
+-- New values: 
+-- Samantha Young, Housekeeping, 4, 900
+-- Roger Palmer, Waiter, 3, 928.33
+-- Submit both INSERT and SELECT queries.
+
---
+++ 2.1.Basic CRUD - Lab/05. Update Salary and Select.sql
@@ -0,0 +1,12 @@
+UPDATE employees
+SET salary = salary + 100
+WHERE job_title = 'Manager';
+
+SELECT * from employees
+WHERE job_title = 'Manager';
+
+-- Update all employees' salaries whose job_title is "Manager" by adding 100. 
+-- Retrieve information from table employees for all managers.
+-- Submit both UPDATE and SELECT queries.
+
+
