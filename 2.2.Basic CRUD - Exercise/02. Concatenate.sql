cc6b46131f709a5bf76676c6ed3e21cd19359805
rsemkov <136321984+rsemkov@users.noreply.github.com>
2024-05-24 09:33:09 +0300

    SQL tasks

---
+++ 2.2.Basic CRUD - Exercise/02. Concatenate.sql
@@ -0,0 +1,6 @@
+SELECT CONCAT_WS(' ', name, state) AS "cities_information", area as "area_km2"
+FROM cities;
+
+-- From the selected already data combine the "name" and "state", 
+-- fields into a single field called "cities_information". Rename the "area" column as "area_km2".
+-- Submit your query for this task in the Judge system.
---
+++ 2.2.Basic CRUD - Exercise/03. Remove Duplicate Rows.sql
@@ -0,0 +1,8 @@
+SELECT DISTINCT "name", area AS "area_km2"
+FROM cities
+ORDER BY "name" DESC;
+
+
+-- Write an SQL query to retrieve DISTINCT city "name", sorting them in descending 
+-- alphabetical order and eliminating duplicates. Do not forget to select the "area" 
+-- column and keep the name of the column the same as in the previous task.
