c88d888291889214f5257f0dbd359c1f6e0f30a2
rsemkov <136321984+rsemkov@users.noreply.github.com>
2024-06-03 09:42:11 +0300

    SQL tasks upload

---
+++ 2.2.Basic CRUD - Exercise/21. ALTER VIEW.sql
@@ -0,0 +1,5 @@
+ALTER VIEW view_addresses 
+	RENAME TO view_employee_addresses_info
+
+
+-- Rename the "view_addresses" to a more relevant name, "view_employee_addresses_info".
---
+++ 2.2.Basic CRUD - Exercise/22. DROP VIEW.sql
@@ -0,0 +1,4 @@
+DROP VIEW view_company_chart
+
+
+-- You can delete the "view_company_chart" since it is no longer necessary.
